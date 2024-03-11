83a0f22db732ee901ac78924ca24b7362ab6f34e
Ethan Kim <ehk003@ucsd.edu>
2024-03-10 18:23:15 -0700

    add main.py file

---
+++ code/main.py
@@ -0,0 +1,20 @@
+import pygame, sys
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            dt = self.clock.tick() / 1000
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
