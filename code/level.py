be8b6f6460c737eb204e12478571b46e9f7df667
Ethan Kim <ehk003@ucsd.edu>
2024-03-11 18:33:47 -0700

    Create basic player class

--- code/level.py
+++ code/level.py
@@ -1,10 +1,19 @@
 import pygame
 from settings import *
+from player import Player
 
 class Level:
     def __init__(self):
+        # get display surface
         self.display_surface = pygame.display.get_surface()
+
+        # sprite group
         self.all_sprites = pygame.sprite.Group()
+
+        self.setup()
+
+    def setup(self):
+        self.player = Player((SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), self.all_sprites)
 
     def run(self, dt):
         self.display_surface.fill('blue')
